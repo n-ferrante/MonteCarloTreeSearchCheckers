97195ec91154a6c6102a3a96a8ba6e997bd2375a
Nick Ferrante <156155345+n-ferrante@users.noreply.github.com>
2024-05-05 20:50:48 -0700

    Create Constants.py

---
+++ Constants.py
@@ -0,0 +1,9 @@
+# Different directions to move
+NORTHEAST = "northeast"
+NORTHWEST = "northwest"
+SOUTHEAST = "southeast"
+SOUTHWEST = "southwest"
+
+# Different turns
+AGENT = "agent"
+OPP = "opposition"
